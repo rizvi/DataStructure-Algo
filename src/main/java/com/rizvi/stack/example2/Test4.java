881674f0508a484429bf26d65a796c5193e69594
rizvi <abu.rizvi@bjitgroup.com>
2017-10-10 10:31:41 +0600

    test files are modified

--- src/main/java/com/rizvi/stack/example2/Test4.java
+++ src/main/java/com/rizvi/stack/example2/Test4.java
@@ -1,9 +1,9 @@
 package com.rizvi.stack.example2;
 
-public class Test3 {
+public class Test4 {
 
 	public static void main(String[] args) {
-		System.out.println("For git testing...3");
+		System.out.println("For git testing...4");
 
 	}
 
